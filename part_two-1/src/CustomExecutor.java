4353df78d8308ee354939a9a48eb47ad4fc2ce5b
davidy1995 <84707578+davidy1995@users.noreply.github.com>
2023-01-13 14:45:45 +0200

    Update CustomExecutor.java

--- part_two-1/src/CustomExecutor.java
+++ part_two-1/src/CustomExecutor.java
@@ -12,18 +12,18 @@ public class CustomExecutor extends ThreadPoolExecutor {
     int maxPriority = 0;
 
     public CustomExecutor() {
-        super(CORE_POOL_SIZE,MAX_POOL_SIZE,KEEP_ALIVE_TIME,TimeUnit.SECONDS,new PriorityBlockingQueue<>());
+        super(CORE_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.SECONDS, new PriorityBlockingQueue<>());
 
 
     }
 
-    /***
+    /*
      * A function that is activated every time an operation is added to the queue or an operation ends
      * @param it - iterator on the task in the queue
      * @param <T>
      * @return - the max priority of the task in the queue
      */
-    public <T> int max(Iterator<Runnable> it){
+    public <T> int max(Iterator<Runnable> it) {
         int max = 0;
         while (it.hasNext()) {
             var a = it.next();
@@ -36,7 +36,7 @@ public class CustomExecutor extends ThreadPoolExecutor {
         return max;
     }
 
-    /***
+    /*
      *
      * @param operation
      * @param <T>
@@ -48,7 +48,7 @@ public class CustomExecutor extends ThreadPoolExecutor {
 
     }
 
-    /***
+    /*
      *
      * @param operation
      * @param taskType
@@ -60,48 +60,54 @@ public class CustomExecutor extends ThreadPoolExecutor {
         return this.submit(task);
     }
 
-    /***
+    /*
      * use the ft addapter to make the task runnable and activate the max func
      * @param task
      * @param <T>
      * @return
      */
-    public <T>Future submit(Task<T> task) {
+    public <T> Future submit(Task<T> task) {
         Ft<T> ft = new Ft<>(task);
         maxPriority = ft.priority;
-        if(getQueue().size()>1)
+        if (getQueue().size() > 1)
             maxPriority = max(getQueue().iterator());
         this.execute(ft);
         return ft;
     }
 
-    /***
+    /*
      * override the afterexecute func that after the task end the max func activated
      * @param r
      * @param t
      */
     @Override
-    protected void afterExecute(Runnable r, Throwable t){
+    protected void afterExecute(Runnable r, Throwable t) {
         Ft ft = (Ft) r;
-        if(getQueue().size()>0)
+        if (getQueue().size() > 0)
             maxPriority = max(getQueue().iterator());
         else maxPriority = 0;
-        super.afterExecute(ft,t);
+        super.afterExecute(ft, t);
     }
 
-    /***
+    /*
      *  use the maxvalue var off the class
      * @return max value
      */
     public String getCurrentMax() {
-        return  Integer.toString(maxPriority);
+        return Integer.toString(maxPriority);
     }
 
-    /***
+    /*
      * shutdown the class while finish all the task in the queue and not recieve new tasks
      */
     public void gracefullyTerminate() {
         this.shutdown();
+        try {
+            if (!this.awaitTermination(300L * getQueue().size(), TimeUnit.MILLISECONDS)) {
+                this.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            this.shutdownNow();
+        }
     }
 }
-
